652fcccff2892dd65e5403568af03b6ead38cf7a
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-07 16:00:20 +0900

    forkIO while holding a value?

--- simple_parallel_calc.hs
+++ simple_parallel_calc.hs
@@ -4,9 +4,14 @@
 -- 3. a thread collects both and show concatenation.
 
 import System.IO
+import Control.Concurrent
 
 showdouble :: [Char] -> IO ()
 showdouble = \x -> putStr $ x ++ x
+
+forkIO' :: IO [Char] -> (IO ThreadId, IO [Char])
+forkIO' = \x -> (forkIO (return ()), x)
+-- yes, we can create a thread. but what can a thread do?
 
 inputline :: IO [Char]
 inputline = hGetLine stdin
