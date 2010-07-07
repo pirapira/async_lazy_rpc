dc43341959b59a54ae1202f62c95d31895a348b6
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-07 16:35:03 +0900

    prepared threads

--- simple_parallel_calc.hs
+++ simple_parallel_calc.hs
@@ -9,11 +9,39 @@ import Control.Concurrent
 showdouble :: [Char] -> IO ()
 showdouble = \x -> putStr $ x ++ x
 
-forkIO' :: IO [Char] -> (IO ThreadId, IO [Char])
-forkIO' = \x -> (forkIO (return ()), x)
--- yes, we can create a thread. but what can a thread do?
+threadOneFirst :: IO [Char]
+threadOneFirst = threadOneInput >>= takeMVar
+
+threadOneSecond :: [Char] -> IO ()
+threadOneSecond str = threadOneOutput >>= (\mvar -> putMVar mvar $ str ++ str)
+
+threadOne :: IO ()
+threadOne = threadOneFirst >>= threadOneSecond
+
+threadOneInput :: IO (MVar [Char])
+threadOneInput = newEmptyMVar
+threadOneOutput :: IO (MVar [Char])
+threadOneOutput = newEmptyMVar
+
+threadTwoFirst :: IO [Char]
+threadTwoFirst = threadTwoInput >>= takeMVar
+
+threadTwoSecond :: [Char] -> IO ()
+threadTwoSecond str = threadOneOutput >>= (\mvar -> putMVar mvar $ reverse str)
+
+threadTwo :: IO ()
+threadTwo = threadTwoFirst >>= threadTwoSecond
+
+threadTwoInput :: IO (MVar [Char])
+threadTwoInput = newEmptyMVar
+threadTwoOutput :: IO (MVar [Char])
+threadTwoOutput = newEmptyMVar
 
 inputline :: IO [Char]
 inputline = hGetLine stdin
 
 main = inputline >>= showdouble
+
+
+
+-- Instead of writing these above, we should be able to 
