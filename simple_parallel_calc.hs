49c5124a747c52dbc1c0f085a9079eca38c49b5d
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-07 15:52:34 +0900

    copied

--- simple_parallel_calc.hs
+++ simple_parallel_calc.hs
@@ -3,3 +3,12 @@
 -- 2. thread two computes (reverse a)
 -- 3. a thread collects both and show concatenation.
 
+import System.IO
+
+showdouble :: [Char] -> IO ()
+showdouble = \x -> putStr $ x ++ x
+
+inputline :: IO [Char]
+inputline = hGetLine stdin
+
+main = inputline >>= showdouble
