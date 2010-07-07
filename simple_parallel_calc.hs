b3c9a69a41c42c272eecd107454a786f7f31c45d
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-07 17:06:03 +0900

    written code. but this blocks.

--- simple_parallel_calc.hs
+++ simple_parallel_calc.hs
@@ -6,9 +6,20 @@
 import System.IO
 import Control.Concurrent
 
-showdouble :: [Char] -> IO ()
-showdouble = \x -> putStr $ x ++ x
+-- Communication Channel
+threadOneInput :: IO (MVar [Char])
+threadOneInput = newEmptyMVar
 
+threadOneOutput :: IO (MVar [Char])
+threadOneOutput = newEmptyMVar
+
+threadTwoInput :: IO (MVar [Char])
+threadTwoInput = newEmptyMVar
+
+threadTwoOutput :: IO (MVar [Char])
+threadTwoOutput = newEmptyMVar
+
+-- ThreadOne
 threadOneFirst :: IO [Char]
 threadOneFirst = threadOneInput >>= takeMVar
 
@@ -18,30 +29,39 @@ threadOneSecond str = threadOneOutput >>= (\mvar -> putMVar mvar $ str ++ str)
 threadOne :: IO ()
 threadOne = threadOneFirst >>= threadOneSecond
 
-threadOneInput :: IO (MVar [Char])
-threadOneInput = newEmptyMVar
-threadOneOutput :: IO (MVar [Char])
-threadOneOutput = newEmptyMVar
-
+-- ThreadTwo
 threadTwoFirst :: IO [Char]
 threadTwoFirst = threadTwoInput >>= takeMVar
 
 threadTwoSecond :: [Char] -> IO ()
-threadTwoSecond str = threadOneOutput >>= (\mvar -> putMVar mvar $ reverse str)
+threadTwoSecond str = threadTwoOutput >>= (\mvar -> putMVar mvar $ reverse str)
 
 threadTwo :: IO ()
 threadTwo = threadTwoFirst >>= threadTwoSecond
 
-threadTwoInput :: IO (MVar [Char])
-threadTwoInput = newEmptyMVar
-threadTwoOutput :: IO (MVar [Char])
-threadTwoOutput = newEmptyMVar
-
+-- Main
 inputline :: IO [Char]
 inputline = hGetLine stdin
 
-main = inputline >>= showdouble
+feedThreadOne :: [Char] -> IO [Char]
+feedThreadOne input = threadOneInput >>= (\mvar -> putMVar mvar input) >> return input
 
+feedThreadTwo :: [Char] -> IO ()
+feedThreadTwo input = threadTwoInput >>= (\mvar -> putMVar mvar input)
 
+waitThreadOne :: () -> IO [Char]
+waitThreadOne = \x -> threadOneOutput >>= takeMVar
+
+waitThreadTwo :: [Char] -> IO ([Char], [Char])
+waitThreadTwo = \one -> (threadTwoOutput >>= takeMVar) >>= \two -> (return (one, two))
+
+output :: ([Char], [Char]) -> IO ()
+output = \(x,y) -> putStr $ x ++ y
+
+main :: IO ()
+main = forkIO threadOne >> forkIO threadTwo >>
+       inputline >>= feedThreadOne >>=
+       feedThreadTwo >>= waitThreadOne >>=
+       waitThreadTwo >>= output
 
 -- Instead of writing these above, we should be able to 
