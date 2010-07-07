f9c82b851fd165d75103aebe20cb43f012492f41
Yoichi Hirai <yh@is.s.u-tokyo.ac.jp>
2010-07-07 18:39:55 +0900

    fixed problem description

--- simple_parallel_calc.hs
+++ simple_parallel_calc.hs
@@ -1,5 +1,5 @@
 --Problem--------------------------------------------------------
--- 1. take two input a from command line
+-- 1. take an input a from stdin
 -- 2. thread one computes (a ++ a)
 -- 2. thread two computes (reverse a)
 -- 3. a thread collects both and show concatenation.
